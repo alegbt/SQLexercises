95fc633cb5722d3e9db670e44e207b5ded1e5822
ale <alessandrogbt94@gmail.com>
2023-02-11 17:22:18 +0100

    ex2 fatto bene

--- src.main.java.it.develhope.exercisesSQL/Ex2_SQL_table.java
+++ src.main.java.it.develhope.exercisesSQL/Ex2_SQL_table.java
@@ -17,14 +17,21 @@ public class Ex2_SQL_table {
 
         try (Connection conn = DriverManager.getConnection(url, user, password)) {
             Statement statement = conn.createStatement();
-            statement.execute("INSERT INTO students2 (student_id, last_name, first_name)\n" +
-                    "VALUES ('1','gob', 'ale');" );
-            statement.execute("INSERT INTO students2 (student_id, last_name, first_name)\n" +
-                    "VALUES ('2','ugo', 'pino');" );
-            statement.execute("INSERT INTO students2 (student_id, last_name, first_name)\n" +
-                    "VALUES ('3','rossi', 'mario');" );
-            statement.execute("INSERT INTO students2 (student_id, last_name, first_name)\n" +
-                    "VALUES ('4','caciotta', 'franco');" );
+            statement.execute("CREATE TABLE IF NOT EXISTS students (\n" +
+                    "    student_id  integer(10) NOT NULL AUTO_INCREMENT,\n" +
+                    "    last_name  varchar(30),\n" +
+                    "    first_name  varchar(30),\n" +
+                    "\tCONSTRAINT students_pk PRIMARY KEY(student_id)\n" +
+                    ");");
+
+            statement.execute("INSERT INTO students (last_name, first_name)\n" +
+                    "VALUES ('gob', 'ale');" );
+            statement.execute("INSERT INTO students (last_name, first_name)\n" +
+                    "VALUES ('ugo', 'pino');" );
+            statement.execute("INSERT INTO students (last_name, first_name)\n" +
+                    "VALUES ('rossi', 'mario');" );
+            statement.execute("INSERT INTO students (last_name, first_name)\n" +
+                    "VALUES ('verdi', 'franco');" );
         }catch(SQLException e) {
             System.out.println(e.getMessage());
         }
